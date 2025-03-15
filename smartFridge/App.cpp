606f64642c2cd9001c85ee91ae2dca9c37dbd53b
Alexandru Bogdan Miron <54797692+bogdan-miron@users.noreply.github.com>
2025-03-15 12:13:45 +0200

    Update App.cpp

--- smartFridge/App.cpp
+++ smartFridge/App.cpp
@@ -2,14 +2,6 @@
 #include <crtdbg.h>
 #include "UI.h"
 
-template<typename T>
-void PrintVector(const Vector<T>& vector) {
-	for (size_t i = 0; i < vector.Size(); ++i) {
-		std::cout << vector[i] << ' ';
-	}
-	std::cout << '\n';
-}
-
 int main() {
 	
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
